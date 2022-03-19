9e6c7358c7ebe4e94ec3f7d2f0a4d5a3da669b3d
Jessica Christie <jessica.christie07@gmail.com>
2022-03-19 19:04:30 +0800

    popup mail

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
 });
+
+$('.mailbox').click(function(){
+    $('.mailbox-menu').fadeToggle(100);
+    $(this).toggleClass('active');
+})
