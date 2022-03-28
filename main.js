d95d3d88d2af43abc11770adf340a664e08b25db
Jessica Christie <jessica.christie07@gmail.com>
2022-03-29 03:30:00 +0800

    add aos js, adj quote, change banner & logo, change/add preload crossorigin etc
    still need loop random, flip, add more layout and post, archive and about page, maybe another front page?

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ $('.hamburger-button').click(function(){
     $(this).toggleClass('active');
 });
 
-$(document).ready(function(){
-    $('.mailbox').click(function(){
-        $('.mailbox-menu').fadeToggle(100);
-        $(this).toggleClass('active');
-    });
+AOS.init({
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    once: false // whether animation should happen only once - while scrolling down
 });
 
 $('.sidebar-button').click(function(){
@@ -16,9 +14,15 @@ $('.sidebar-button').click(function(){
 });
 
 $(document).ready(function(){
+    $('.mailbox').click(function(){
+        $('.mailbox-menu').fadeToggle(100);
+        $(this).toggleClass('active');
+    });
+
     $('.toggle-container').click(function(){
         $('section').toggleClass('dark-mode-active');
         $('.attribute').toggleClass('dark-mode-active');
+        $('hr').toggleClass('dark-mode-active');
         $('.dm-style').toggleClass('dark-mode-active');
         $(this).toggleClass('dark-mode-active');  
       });
