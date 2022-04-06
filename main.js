1012ad88e621dcaa1323327587ba8c3cee8822e9
Jessica Christie <jessica.christie07@gmail.com>
2022-04-06 20:49:26 +0800

    hid arrow xs on web & add js, change burger icon, add x on mailbox menu, padding on blog desc, add logo on sidebar widget, tidy z-index

--- main.js
+++ main.js
@@ -1,63 +1,68 @@
 $('.hamburger-button').click(function(){
-    $('.mobile-menu').fadeToggle(100);
-    $(this).toggleClass('active');
+  $('.mobile-menu').fadeToggle(100);
+  $(this).toggleClass('active');
 });
 
 AOS.init({
-    duration: 1000, // values from 0 to 3000, with step 50ms
-    once: false // whether animation should happen only once - while scrolling down
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  once: false // whether animation should happen only once - while scrolling down
 });
 
 $('.sidebar-button').click(function(){
-    $('.sidebar-button').toggleClass("nav-open");
-    $('.sidebar-menu').slideToggle(10, function(){});
+  $('.sidebar-button').toggleClass("nav-open");
+  $('.sidebar-menu').slideToggle(10, function(){});
 });
 
 $(document).ready(function(){
-    $('.mailbox').click(function(){
-        $('.mailbox-menu').fadeToggle(100);
-        $(this).toggleClass('active');
+  $('.mailbox').click(function(){
+    $('.mailbox-menu').fadeToggle(100);
+    $(this).toggleClass('active');
+  });
+
+  $('.xmark').click(function(){
+    $(this).toggleClass('active');
+    $('.mailbox-menu').fadeOut(500);
+  });
+
+  $('.toggle-container').click(function(){
+    $('section').toggleClass('dark-mode-active');
+    $('.attribute').toggleClass('dark-mode-active');
+    $('hr').toggleClass('dark-mode-active');
+    $('.dm-style').toggleClass('dark-mode-active');
+    $(this).toggleClass('dark-mode-active');  
     });
 
-    $('.toggle-container').click(function(){
-        $('section').toggleClass('dark-mode-active');
-        $('.attribute').toggleClass('dark-mode-active');
-        $('hr').toggleClass('dark-mode-active');
-        $('.dm-style').toggleClass('dark-mode-active');
-        $(this).toggleClass('dark-mode-active');  
-      });
-
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        speed: 1000,
-        asNavFor: '.slider-nav'
-    });
-      
-    $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        infinite: true,
-        accessibility: true,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true,
-        responsive: [
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              fade: true,
-              speed: 750,
-              width: 576
-            }
-          }
-        ]
-    });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    asNavFor: '.slider-nav'
+  });
+    
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    accessibility: true,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true,
+          speed: 750,
+          width: 576
+        }
+      }
+    ]
+  });
 
 });
